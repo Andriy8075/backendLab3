c1368bc53aa8b3adfd125158dab1d2e8935eb9b2
andriy8075 <andriy8075@gmail.com>
2025-10-04 11:15:28 +0300

    add healthcehck endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,14 @@
 from flask import jsonify
 from app import app
+from datetime import datetime
+
+@app.route('/healthcheck', methods=['GET'])
+def healthcheck():
+    """Healthcheck endpoint that returns current date and service status"""
+    return jsonify({
+        'status': 'healthy',
+        'timestamp': datetime.utcnow().isoformat() + 'Z',
+    }), 200
 
 @app.route('/')
 def hello():

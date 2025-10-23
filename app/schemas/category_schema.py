c506675f0a852f486e69a98064fbb76007627831
andriy8075 <andriy8075@gmail.com>
2025-10-23 15:16:41 +0300

    add valudation for user_id and category_id

--- app/schemas/category_schema.py
+++ app/schemas/category_schema.py
@@ -1,5 +1,6 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields, validate, validates_schema, ValidationError
 from app.config.models_validation.category import name_max_length
+from app.models.user import User
 
 
 class CategorySchema(Schema):
@@ -14,3 +15,11 @@ class CategoryCreateSchema(Schema):
     """Schema for creating a new category"""
     name = fields.Str(required=True, validate=validate.Length(min=1, max=name_max_length))
     user_id = fields.Int(required=True, validate=validate.Range(min=1))
+    
+    @validates_schema
+    def validate_user_exists(self, data, **kwargs):
+        """Validate that the user with the specified user_id exists"""
+        if 'user_id' in data:
+            user = User.query.get(data['user_id'])
+            if user is None:
+                raise ValidationError('User with specified ID does not exist', field_name='user_id')
--- app/schemas/record_schema.py
+++ app/schemas/record_schema.py
@@ -1,6 +1,8 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields, validate, validates_schema, ValidationError
 from datetime import datetime
 from app.config.models_validation.record import min_sum
+from app.models.user import User
+from app.models.category import Category
 
 
 class RecordSchema(Schema):
@@ -20,3 +22,18 @@ class RecordCreateSchema(Schema):
     category_id = fields.Int(required=True, validate=validate.Range(min=1))
     sum = fields.Int(required=True, validate=validate.Range(min=min_sum))
     date_time = fields.DateTime(load_default=datetime.utcnow, allow_none=True)
+    
+    @validates_schema
+    def validate_user_and_category_exist(self, data, **kwargs):
+        """Validate that both user and category with specified IDs exist"""
+        # Check if user exists
+        if 'user_id' in data:
+            user = User.query.get(data['user_id'])
+            if user is None:
+                raise ValidationError('User with specified ID does not exist', field_name='user_id')
+        
+        # Check if category exists
+        if 'category_id' in data:
+            category = Category.query.get(data['category_id'])
+            if category is None:
+                raise ValidationError('Category with specified ID does not exist', field_name='category_id')
